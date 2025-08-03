cfff624f0a42889fb986f66b9f04db7a6076ae2e
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-03 22:13:03 +0700

    tambah parser

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const connection = require("./controller/conn");
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API Ready!");
 });
