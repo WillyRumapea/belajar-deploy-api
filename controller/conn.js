54298f30b0778868cbc631808a2712ece20f5bb9
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-03 21:19:05 +0700

    connect ke db

--- controller/conn.js
+++ controller/conn.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 
 const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+
+const connection = mysql.createConnection(process.env.DB_URL);
 
 module.exports = connection;
