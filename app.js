708b335fe527b3d7c34f83c5fdb01857c0dc7ad3
WillyRumapea <willyrumapea123jr@gmail.com>
2025-08-05 20:36:10 +0700

    tambah cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const connection = require("./controller/conn");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
